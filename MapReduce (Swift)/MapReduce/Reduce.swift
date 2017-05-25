0267efe03318c51b5e9d830f62155b5ca3bd4fe1
Charlie Imhoff <cpimhoff@gmail.com>
2017-05-24 21:57:03 -0500

    removed outdated comment

--- MapReduce (Swift)/MapReduce/Reduce.swift
+++ MapReduce (Swift)/MapReduce/Reduce.swift
@@ -8,13 +8,6 @@
 
 import Foundation
 
-//
-//	this implementation of `reduce` is bad... very bad.
-//	additionally, it has a variety of race conditions and bad access
-//
-// 	so... a draft of a draft.
-//
-
 public func reduce<Source:DataSource, Result>
 	(_ datasource: Source, initialResult: Result,
 	 with baseValue: @escaping (Source.DataPoint) -> Result,
