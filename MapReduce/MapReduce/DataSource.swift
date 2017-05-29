44b902195a32d0e39a7e275962caa59337aea50c
Charlie Imhoff <cpimhoff@gmail.com>
2017-05-28 22:48:46 -0500

    move diretories

---
+++ MapReduce/MapReduce/DataSource.swift
@@ -0,0 +1,29 @@
+//
+//  DataSource.swift
+//  MapReduce
+//
+//  Created by Charlie Imhoff on 5/21/17.
+//  Copyright © 2017 Charlie Imhoff. All rights reserved.
+//
+
+import Foundation
+
+/// Abstraction over input data structures
+/// because DataSource is used only for reading input values,
+/// a non-thread-safe implementation (such as the standard `Array`) is acceptable.
+public protocol DataSource {
+	
+	associatedtype DataPoint
+	
+	/// The amount of datapoints represented by the reciever.
+	var count : Int { get }
+	
+	/// Index the reciver with the first element at index `0`, and the last at index `self.count - 1`
+	subscript(index: Int) -> DataPoint { get }
+	
+}
+
+// explicitly comform `Array` and `SynchronizedArray` to `DataSource`
+// (they already has all the protocol requirements implemented)
+extension Array : DataSource {}
+extension SynchronizedArray : DataSource {}
