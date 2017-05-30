ee3f0f2b3a1859e096ce8ab8e8d5da00e7b1b38f
bwithbroe <withbroeb@carleton.edu>
2017-05-29 23:17:32 -0500

    add shitty brute force knn, to be improved later

--- MapReduce/KNN/KNN_main.swift
+++ MapReduce/KNN/KNN_main.swift
@@ -9,6 +9,23 @@
 import Foundation
 import MapReduce
 
+let K : Int = 10
+
 func main(k: Int) {
 	printToAppConsole("Hello World! \(k)")
+}
+
+// Brute force kNN for a single point, it's garbage but for now we just need a
+// proof of concept
+func knn(point: Point, data: Dataset, k: Int) -> [(label: Int, dist: Float)] {
+    var cd = [(label: Int, dist: Float)]()
+    
+    // find distance to each point
+    for train_point in data {
+        cd.append((point.label, point - train_point))
+    }
+    
+    // find the k closest points
+    cd.sort(by: { p1, p2 in p1.dist > p2.dist })
+    return Array(cd.prefix(k))
 }
