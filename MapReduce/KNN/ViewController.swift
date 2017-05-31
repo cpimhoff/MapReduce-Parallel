aa5d1705edcbad058ed350971d9b46bc97308356
Charlie Imhoff <cpimhoff@gmail.com>
2017-05-30 23:28:07 -0500

    run KNN off the UI thread to prevent beach ball

--- MapReduce/KNN/ViewController.swift
+++ MapReduce/KNN/ViewController.swift
@@ -11,8 +11,10 @@ class ViewController: NSViewController {
 	
 	@IBOutlet weak var textBox : NSTextField!
 	@IBOutlet weak var kField : NSTextField!
+	@IBOutlet weak var progressIndicator : NSProgressIndicator!
 
 	override func viewDidLoad() {
+		self.progressIndicator.isHidden = true
 		ViewController.consoleTextBox = self.textBox
 	}
 	
@@ -20,7 +22,20 @@ class ViewController: NSViewController {
 		clearAppConsole()
 		
 		if let k = Int(kField.stringValue), k > 0 {
-			main(k: k)
+			
+			progressIndicator.isHidden = false
+			progressIndicator.startAnimation(self)
+			(sender as? NSButton)!.isEnabled = false
+			
+			DispatchQueue.global(qos: .userInitiated).async {
+				main(k: k)
+				
+				DispatchQueue.main.sync {
+					self.progressIndicator.isHidden = true
+					self.progressIndicator.stopAnimation(self)
+					(sender as? NSButton)!.isEnabled = true
+				}
+			}
 		} else {
 			printToAppConsole("K must be a positive Integer")
 		}
@@ -29,10 +44,23 @@ class ViewController: NSViewController {
 }
 
 func clearAppConsole() {
-	ViewController.consoleTextBox.stringValue = ""
+	if Thread.isMainThread {
+		ViewController.consoleTextBox.stringValue = ""
+	} else {
+		DispatchQueue.main.sync {
+			ViewController.consoleTextBox.stringValue = ""
+		}
+	}
 }
 
 func printToAppConsole(_ obj: Any) {
 	let description = String(describing: obj)
-	ViewController.consoleTextBox.stringValue += (description + "\n")
+	
+	if Thread.isMainThread {
+		ViewController.consoleTextBox.stringValue += (description + "\n")
+	} else {
+		DispatchQueue.main.sync {
+			ViewController.consoleTextBox.stringValue += (description + "\n")
+		}
+	}
 }
