5ad4a8c0f9062c6a28c6337bf7c397a83b2a8328
bwithbroe <withbroeb@carleton.edu>
2017-05-31 01:11:11 -0500

    conform MappedSet to Collection instead of Sequence

--- MapReduce/KNN/MappedSet.swift
+++ MapReduce/KNN/MappedSet.swift
@@ -19,49 +19,21 @@ class MappedSet {
 }
 
 // Conform this structure to `DataSource` so our MapReduce framework can use it directly
-extension MappedSet : DataSource, Sequence {
+extension MappedSet : Collection {
     
-    /// The amount of datapoints represented by the reciever.
-    var count : Int {
-        return self.points.count
+    var startIndex: Int {
+        return self.points.startIndex
     }
     
-    /// Index the receiver with the first element at index `0`, and the last at index `self.count - 1`
+    var endIndex : Int {
+        return self.points.endIndex
+    }
+    
+    func index(after i: Int) -> Int {
+        return i + 1
+    }
+    
     subscript(index: Int) -> [MappedPoint] {
         return self.points[index]
-    }
-    
-    /// Index the receiver with the first element at index `0`, and the last at index `self.count - 1`, returning an ArraySlice<Point> over a provided Range<Int> of indices
-    subscript(subRange: CountableRange<Int>) -> ArraySlice<[MappedPoint]> {
-        return self.points[subRange]
-    }
-    
-    /// Index the receiver with the first element at index `0`, and the last at index `self.count - 1`, returning an ArraySlice<Point> over a provided Range<Int> of indices
-    subscript(subRange: CountableClosedRange<Int>) -> ArraySlice<[MappedPoint]> {
-        return self.points[subRange]
-    }
-
-    /// An iterator over the MappedPoints in the Dataset
-    func makeIterator() -> MappedSetIterator {
-        return MappedSetIterator(mappedSet: self)
-    }
-}
-
-// Iterator so that Dataset can conform to `Sequence`
-struct MappedSetIterator : IteratorProtocol {
-    var mappedSet: MappedSet
-    var index = 0
-    
-    init(mappedSet: MappedSet) {
-        self.mappedSet = mappedSet
-    }
-    
-    mutating func next() -> [MappedPoint]? {
-        if index < mappedSet.count - 1 {
-            index += 1
-            return mappedSet[index]
-        } else {
-            return nil
-        }
     }
 }
