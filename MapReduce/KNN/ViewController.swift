0bf786208c7bbedc6a900bd66af8e5c64ce180e4
Charlie Imhoff <cpimhoff@gmail.com>
2017-05-30 23:48:10 -0500

    scrolling text box

--- MapReduce/KNN/ViewController.swift
+++ MapReduce/KNN/ViewController.swift
@@ -7,23 +7,23 @@ import Cocoa
 
 class ViewController: NSViewController {
 
-	static var consoleTextBox : NSTextField!
+	static var consoleTextBox : NSTextView!
 	
-	@IBOutlet weak var textBox : NSTextField!
+	@IBOutlet weak var textBox : NSTextView!
 	@IBOutlet weak var kField : NSTextField!
 	@IBOutlet weak var progressIndicator : NSProgressIndicator!
 
 	override func viewDidLoad() {
-		self.progressIndicator.isHidden = true
 		ViewController.consoleTextBox = self.textBox
+		self.textBox.string = ""
+		self.progressIndicator.isDisplayedWhenStopped = false
 	}
 	
 	@IBAction func runKNN(_ sender: Any?) {
 		clearAppConsole()
 		
 		if let k = Int(kField.stringValue), k > 0 {
-			
-			progressIndicator.isHidden = false
+		
 			progressIndicator.startAnimation(self)
 			(sender as? NSButton)!.isEnabled = false
 			
@@ -31,7 +31,6 @@ class ViewController: NSViewController {
 				main(k: k)
 				
 				DispatchQueue.main.sync {
-					self.progressIndicator.isHidden = true
 					self.progressIndicator.stopAnimation(self)
 					(sender as? NSButton)!.isEnabled = true
 				}
@@ -45,10 +44,10 @@ class ViewController: NSViewController {
 
 func clearAppConsole() {
 	if Thread.isMainThread {
-		ViewController.consoleTextBox.stringValue = ""
+		ViewController.consoleTextBox.string = ""
 	} else {
 		DispatchQueue.main.sync {
-			ViewController.consoleTextBox.stringValue = ""
+			ViewController.consoleTextBox.string = ""
 		}
 	}
 }
@@ -57,10 +56,10 @@ func printToAppConsole(_ obj: Any) {
 	let description = String(describing: obj)
 	
 	if Thread.isMainThread {
-		ViewController.consoleTextBox.stringValue += (description + "\n")
+		ViewController.consoleTextBox.string! += (description + "\n")
 	} else {
 		DispatchQueue.main.sync {
-			ViewController.consoleTextBox.stringValue += (description + "\n")
+			ViewController.consoleTextBox.string! += (description + "\n")
 		}
 	}
 }
